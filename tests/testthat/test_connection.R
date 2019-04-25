1ca85b2eef1616ae8fcb1f775c6a9fb7c5e6e0ca
vh-d <vencahaus@seznam.cz>
2019-04-25 15:22:30 +0200

    add tests of do.call_remote

--- tests/testthat/test_connection.R
+++ tests/testthat/test_connection.R
@@ -15,3 +15,13 @@ test_that("Expression can be evaluated", {
   expect_equal(r2r::eval_remote(1+a, data = list(a = 2)), 3)
   expect_true(stop_server())
 })
+
+
+test_that("Functions can be called", {
+  r2r::start_server_locally()
+  expect_equal(r2r::do.call_remote("sum", args_remote = list(a = 1, b = 1)), 2)
+  expect_equal(r2r::do.call_remote(sum, args_remote = list(a = 1, b = 1), quote = TRUE), 2)
+  expect_error(r2r::do.call_remote("sum", args_remote = list(a = 1, b = "A"), quote = FALSE))
+  expect_error(r2r::do.call_remote(sum, args_remote = list(a = 1, b = "A"), quote = TRUE))
+  expect_true(r2r::stop_server())
+})
